401398c76a93c5341ad7f230f79d5cdf2ad5f96d
Rory Ainsworth <rory.ainsworth@yokogawa.com>
2025-11-14 12:33:39 +0000

    progress porting

--- src/lgpio_port.cpp
+++ src/lgpio_port.cpp
@@ -1,29 +1,84 @@
 #include "lgpio_port.h"
 
+static int gpio_handle = -1;
 
-// enum iostate {
-//     LOW = 0,
-//     HIGH = 1
-// };
-// enum mode{
-//     INPUT,
-//     OUTPUT,
-//     INPUT_PULLUP
-// };
+static bool gpio_check()
+{
+    if (gpio_handle < 0)
+    {
+        gpio_handle = lgGpiochipOpen(0);
+        if (gpio_handle >= 0)
+            return true;
+        else 
+            std::cout << "GPIO FAILIURE TO OPEN CHIP. ret = " << gpio_handle << std::endl; 
 
-// typedef uint8_t byte;
+    }
+    else
+    {
+        return true;
+    }
+
+}
+
+static bool lg_ret_check(const int retval)
+{
+    if (retval >= 0)
+        return true;
+    else
+    {
+        std::cout << "RET FAIL : RET = " << retval << std::endl;
+        return false;
+    }
+
+}
 
 iostate digitalRead(int gpio_pin)
 {
+    if (!gpio_check())
+        return LOW;
+
+
 
 }
 
 void digitalWrite(int gpio_pin, iostate = LOW)
 {
+    if (!gpio_check())
+        return;
 
 }
 
-void pinMode(int gpio_pin, mode)
+void pinMode(int gpio_pin, mode pin_mode)
 {
+    if (!gpio_check())
+        return;
+
+    switch (pin_mode)
+    {
+    case (INPUT):
+    {
+        lg_ret_check(lgGpioClaimInput(gpio_handle, LG_SET_PULL_NONE, gpio_pin));
+        break;
+    }
+    case (OUTPUT):
+    {
+        lg_ret_check(lgGpioClaimOutput(gpio_handle, LG_SET_PULL_NONE, gpio_pin, 0));
+    }
+    case (INPUT_PULLUP):
+    {
+        lg_ret_check(lgGpioClaimInput(gpio_handle, LG_SET_PULL_UP, gpio_pin));
+    }
+    default:
+    {
+        break;
+    }
+
+    }
+    
+
 
 }
+
+unsigned long millis(){}
+void noInterrupts(void){}
+void interrupts(void){}
--- src/lgpio_port.h
+++ src/lgpio_port.h
@@ -1,14 +1,19 @@
 #ifndef __LGPIO_PORT_H_
 #define __LGPIO_PORT_H_
 
-#include <cstdint>
-#include <lgpio>
+#include <lgpio.h>
 
-static enum iostate {
+#include <cstdint>
+#include <chrono>
+#include <thread>
+#include <iostream>
+
+
+enum iostate {
     LOW = 0,
     HIGH = 1
 };
-static enum mode{
+enum mode{
     INPUT,
     OUTPUT,
     INPUT_PULLUP
@@ -16,8 +21,12 @@ static enum mode{
 
 typedef uint8_t byte;
 
+// Arduino Functions 
 iostate digitalRead(int gpio_pin);
 void digitalWrite(int gpio_pin, iostate = LOW);
 void pinMode(int gpio_pin, mode);
-
+void delay(int ms);
+unsigned long millis();
+void noInterrupts(void);
+void interrupts(void);
 #endif
